b7a6e912fa5111d400f7f30883548b7d6d7d3eb1
Yaduttam95 <yaduttampareek95@gmail.com>
2023-11-21 21:27:10 +0530

    app to api

--- converted.py
+++ converted.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 import google.generativeai as palm
-import pprint
 
 app = Flask(__name__)
 
@@ -26,4 +25,4 @@ def generate_text():
     return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(host="192.168.1.8", port=5000)
+    app.run(host="0.0.0.0", port=5000)
